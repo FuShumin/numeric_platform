253c123b7f623c4cf17dc3737c4c0f55b98a20d3
ShuminHome <shuminfu99@gmail.com>
2023-11-16 00:14:50 +0800

    v_home

--- lp.py
+++ lp.py
@@ -1,8 +1,11 @@
 # /path/to/optimization_model.py
+import random
+
 import pandas as pd
 import numpy as np
 from scipy.optimize import linprog
 from itertools import permutations
+from pulp import LpProblem, LpMinimize, LpVariable, lpSum, LpStatus, LpInteger, value
 import pulp
 from dataclasses import dataclass
 from typing import List
@@ -27,72 +30,70 @@ class Warehouse:
     docks: List[Dock]
 
 
-def create_lp_model(orders: List[Order], warehouses: List[Warehouse]):
-    model = pulp.LpProblem("Vehicle_Scheduling", pulp.LpMinimize)
+def create_lp_model(orders, warehouses):
+    # 从仓库中提取所有月台
+    docks = [dock for warehouse in warehouses for dock in warehouse.docks]
 
-    # 创建变量
+    # 初始化问题
+    model = LpProblem("Vehicle_Scheduling_with_Queue", LpMinimize)
+
+    # 决策变量
     x = pulp.LpVariable.dicts("OrderWarehouseDock",
-                              (range(len(orders)), range(len(warehouses)), range(len(warehouses[0].docks))),
+                              ((order.order_id, warehouse.warehouse_id, dock.dock_id)
+                               for order in orders
+                               for warehouse in warehouses
+                               for dock in warehouse.docks),
                               cat='Binary')
+    start_times = LpVariable.dicts("Start_Time",
+                                   ((order.order_id, dock.dock_id) for order in orders for dock in docks),
+                                   lowBound=0, cat=LpInteger)
+    end_times = LpVariable.dicts("End_Time",
+                                 ((order.order_id, dock.dock_id) for order in orders for dock in docks),
+                                 lowBound=0, cat=LpInteger)
 
-    # 目标函数
-    model += pulp.lpSum([x[o][w][d] * orders[o].warehouse_loads[w] / warehouses[w].docks[d].loading_efficiency
-                         for o in range(len(orders))
-                         for w in range(len(warehouses))
-                         for d in range(len(warehouses[w].docks))])
+    # 目标函数 - 最小化所有订单的结束时间
+    model += lpSum([end_times[order.order_id, dock.dock_id] for order in orders for dock in docks])
 
-    # 约束条件
+    # 约束
     # 每个订单必须在每个仓库的一个月台装货
-    for o in range(len(orders)):
-        for w in range(len(warehouses)):
-            model += pulp.lpSum([x[o][w][d] for d in range(len(warehouses[w].docks))]) == 1
+    for order in orders:
+        for warehouse in warehouses:
+            model += lpSum([x[order.order_id, warehouse.warehouse_id, dock.dock_id] for dock in warehouse.docks]) == 1
 
-    # 每个月台同时只能有一辆车作业
-    for w in range(len(warehouses)):
-        for d in range(len(warehouses[w].docks)):
-            model += pulp.lpSum([x[o][w][d] for o in range(len(orders))]) <= 1
+    # 装货时间约束
+    for order in orders:
+        for dock in docks:
+            load_time = order.warehouse_loads[warehouse.warehouse_id] / dock.loading_efficiency
+            model += end_times[order.order_id, dock.dock_id] >= start_times[order.order_id, dock.dock_id] + load_time
 
-    # 求解模型
-    model.solve()
+    # 排队约束
+    for dock in docks:
+        for i, o1 in enumerate(orders):
+            for o2 in orders[i + 1:]:
+                model += start_times[o2.order_id, dock.dock_id] >= end_times[o1.order_id, dock.dock_id]
 
     return model
 
 
-class Order:
-    def __init__(self, order_id, warehouse_loads, priority):
-        self.order_id = order_id
-        self.warehouse_loads = warehouse_loads
-        self.priority = priority
-
-
-class Warehouse:
-    def __init__(self, warehouse_id, docks):
-        self.warehouse_id = warehouse_id
-        self.docks = docks
-
-
-class Dock:
-    def __init__(self, dock_id, loading_efficiency):
-        self.dock_id = dock_id
-        self.loading_efficiency = loading_efficiency
-
-
-def parse_optimization_result(result, orders, warehouses):
+def parse_optimization_result(model, orders, warehouses):
     order_warehouse_dock = {}
     timestamps = {}
 
-    for o in range(len(orders)):
-        order_warehouse_dock[o] = []
-        timestamps[o] = 0
+    for order in orders:
+        order_warehouse_dock[order.order_id] = []
+        timestamps[order.order_id] = 0
 
-        for w in range(len(warehouses)):
-            for d in range(len(warehouses[w].docks)):
-                var_key = f"OrderWarehouseDock_{o}_{w}_{d}"
-                if pulp.value(result.variablesDict()[var_key]) == 1:
-                    order_warehouse_dock[o].append((w, d))
-                    timestamps[o] += orders[o].warehouse_loads[w] / warehouses[w].docks[d].loading_efficiency
+        for warehouse in warehouses:
+            for dock in warehouse.docks:
+                var_key = (order.order_id, warehouse.warehouse_id, dock.dock_id)
+                if pulp.value(model.variablesDict()[var_key]) == 1:
+                    order_warehouse_dock[order.order_id].append((warehouse.warehouse_id, dock.dock_id))
+                    load_time = order.warehouse_loads[warehouse.warehouse_id] / dock.loading_efficiency
+                    timestamps[order.order_id] += load_time
 
     return order_warehouse_dock, timestamps
+
+
 
 
 orders = [
@@ -109,6 +110,36 @@ warehouses = [
     # ...其他仓库
 ]
 
-result = create_lp_model(orders, warehouses)
-order_warehouse_dock, timestamps = parse_optimization_result(result, orders, warehouses)
 
+def generate_test_data(num_orders, num_warehouses, num_docks_per_warehouse):
+    orders = [Order(order_id=i, warehouse_loads=[random.randint(10, 100) for _ in range(num_warehouses)],
+                    priority=random.randint(1, 3)) for i in range(num_orders)]
+
+    docks = [Dock(dock_id=j, loading_efficiency=random.uniform(0.5, 1.5)) for j in range(num_docks_per_warehouse)]
+    warehouses = [Warehouse(warehouse_id=i, docks=docks) for i in range(num_warehouses)]
+
+    return orders, warehouses
+
+
+def main():
+    num_orders = 15  # 订单数量
+    num_warehouses = 3  # 仓库数量
+    num_docks_per_warehouse = 4  # 每个仓库的月台数量
+
+    orders, warehouses = generate_test_data(num_orders, num_warehouses, num_docks_per_warehouse)
+
+    model = create_lp_model(orders, warehouses)
+    model.solve()
+
+    order_warehouse_dock, timestamps = parse_optimization_result(model, orders, warehouses)
+
+    # 打印结果
+    for order_id in order_warehouse_dock:
+        print(f"Order {order_id}:")
+        print("  Warehouse-Dock assignments:", order_warehouse_dock[order_id])
+        print("  Total loading time:", timestamps[order_id], "time units")
+        print()
+
+
+if __name__ == "__main__":
+    main()
